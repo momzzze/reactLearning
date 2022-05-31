c8f2d723da4504b6d7636b4e59605c1f0f1da3f4
Nikola Ninov <momzzze@abv.bg>
2022-05-31 20:46:10 +0300

    fixing

--- BackEnd/workshop1/workshopCubicle/src/index.js
+++ BackEnd/workshop1/workshopCubicle/src/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const port = 5000;
-
+const routes = require('./routes');
 const app = express();
-app.use('/static', express.static('src/public'));
+
+
+
+app.use('/static', express.static('public'));
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views','./src/views');
-
-app.get('/', (req, res) => {
-    res.render('./');
-});
-
+app.set('views', './src/views');
+app.use(routes);
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
