c28753e4d17f4a15802cacb2fd1f41b92392642c
Nikola Ninov <momzzze@abv.bg>
2022-05-31 18:37:41 +0300

    +

--- BackEnd/workshop1/workshopCubicle/src/index.js
+++ BackEnd/workshop1/workshopCubicle/src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const port = 5000;
 
 const app = express();
+app.use('/static', express.static('src/public'));
 
-app.use('/static', express.static('public'));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views','./src/views');
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('./');
 });
 
 
