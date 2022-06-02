16ff67df87be7f7fa35d28dc7d26301a4132fbeb
Nikola Ninov <momzzze@abv.bg>
2022-06-02 14:28:37 +0300

    s

--- BackEnd/workshop1/workshopCubicle/src/controllers/homeController.js
+++ BackEnd/workshop1/workshopCubicle/src/controllers/homeController.js
@@ -1,3 +1,7 @@
 exports.index = (req, res) => {
     res.render('./');
-}
+};
+
+exports.about = (req, res) => {
+    res.render('about');
+};
--- BackEnd/workshop1/workshopCubicle/src/routes.js
+++ BackEnd/workshop1/workshopCubicle/src/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 router.get('/', homeController.index);
+router.get('/about', homeController.about);
 
+router.use('/cube', cubeController);
 module.exports = router;
