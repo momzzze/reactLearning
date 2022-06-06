5ceb79917234597b44cea637f07a55ca007d2440
Nikola Ninov <momzzze@abv.bg>
2022-06-06 19:39:37 +0300

    add search

--- BackEnd/workshop1/workshopCubicle/src/services/cubeService.js
+++ BackEnd/workshop1/workshopCubicle/src/services/cubeService.js
@@ -5,6 +5,12 @@ const path = require('path');
 
 exports.getOne = (cubeId) => cubes[cubeId];
 
+exports.getAll = (search = '', from = 0, to = 6) => {
+    const result = cubes.filter(x => x.name.toLowerCase().includes(search?.toLowerCase()))
+        .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
+
+    return result;
+};
 
 exports.save = (cube) => {
     cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
