e1ba8fd563cc880e831099636bd1ded6c758e648
Nikola Ninov <momzzze@abv.bg>
2022-06-18 14:11:35 +0300

    add dynamic accessory select

--- BackEnd/NoSQL&MongoDB/workshop2/src/controllers/cubeController.js
+++ BackEnd/NoSQL&MongoDB/workshop2/src/controllers/cubeController.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService');
-
+const accessoryService = require('../services/accessoryService');
 router.get('/create', (req, res) => {
     res.render('create');
 });
@@ -30,8 +30,11 @@ router.get('/details/:id', async (req, res) => {
 
     res.render('details', { cube });
 });
-router.get('/:cubeId/attach-accessory', (req, res) => {
-    res.render('accessory/attach');
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryService.getAll().lean();
+
+    res.render('accessory/attach', { cube, accessories });
 })
 
 module.exports = router;
--- BackEnd/NoSQL&MongoDB/workshop2/src/services/accessoryService.js
+++ BackEnd/NoSQL&MongoDB/workshop2/src/services/accessoryService.js
@@ -3,4 +3,6 @@ const path = require('path');
 
 const Accessory = require('../models/Accessory');
 
+exports.getAll = () => Accessory.find();
+
 exports.create = (accessoryData) => Accessory.create(accessoryData);
