bc2c40c7f6bcce76854a59dbb97a170198803937
Nikola Ninov <momzzze@abv.bg>
2022-06-28 10:17:42 +0300

    s

---
+++ reactSU/intro/src/script.js
@@ -0,0 +1,16 @@
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+// const reactElement = React.createElement(
+//     'header',
+//     { className: 'site-header' },
+//     React.createElement('h1', {}, 'Hello from react'),
+//     React.createElement('h2', {}, 'React slogan'),
+// );
+
+const reactElement = <header className="site-header">
+    <h1>Hello from jsx!!!</h1>
+    <h2>React slogan</h2>
+</header>;
+
+root.render(reactElement);
