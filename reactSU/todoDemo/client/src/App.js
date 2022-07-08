cef3cb69ae9befe7f794aa7d0dc74703b66595a6
Nikola Ninov <momzzze@abv.bg>
2022-07-08 13:00:39 +0300

    components

---
+++ reactSU/todoDemo/client/src/App.js
@@ -0,0 +1,34 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Spinner } from "./components/Spinner";
+import { TodoList } from "./components/TodoList";
+
+function App() {
+  return (
+    <div>
+
+      <Header />
+
+
+      <main className="main">
+        {/* <Spinner/> */}
+
+        <section className="todo-list-container">
+          <h1>Todo List</h1>
+
+          <div className="add-btn-container">
+            <button className="btn">+ Add new Todo</button>
+          </div>
+          <div className="table-wrapper">
+            <TodoList />
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ reactSU/todoDemo/client/src/components/TodoItem.js
@@ -0,0 +1,18 @@
+export const TodoItem = (props) => {
+    let className = 'todo';
+
+    if (props.isCompleted) {
+        className += ' is-completed';
+    }
+
+    return (
+        <tr className={className}>
+            <td>{props.title}</td>
+            <td>{props.isCompleted ? 'Complete' : 'Incomplete'}</td>
+            <td className="todo-action">
+                <button onClick={() => props.onClick(props)} className="btn todo-btn">Change status</button>
+            </td>
+        </tr>
+
+    );
+};
