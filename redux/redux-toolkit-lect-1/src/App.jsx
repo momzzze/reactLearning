367d2332076ad70ee3dd369db16d0666d3ee10ea
nikola.ninov <momzzze@abv.bg>
2023-12-17 11:55:00 +0200

    lecture-1
    Simple structure on the redux store with one Slice and 4 functions
    exercise using useSelector useDispatch and payload actions on the input

---
+++ redux/redux-toolkit-lect-1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+import Counter from './features/counter/Counter';
+
+function App() {
+
+  return (
+    <main className='App'>
+     <Counter/>
+    </main>
+  )
+}
+
+export default App
