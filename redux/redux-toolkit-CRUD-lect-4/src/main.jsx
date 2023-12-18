0733879dc0fb7d3225e1c0049b9c5a759b4875fc
nikola.ninov <momzzze@abv.bg>
2023-12-18 13:30:14 +0200

    add create, update, delete ,read operations with redux

---
+++ redux/redux-toolkit-CRUD-lect-4/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { fetchUsers } from './features/users/usersSlice';
+import { fetchPosts } from './features/posts/postsSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+store.dispatch(fetchUsers());
+store.dispatch(fetchPosts());
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
+  </Provider>,
+)
