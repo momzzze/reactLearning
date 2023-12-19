fdaf71924048626cb40c3b6de01a4af484277d77
nikola.ninov <momzzze@abv.bg>
2023-12-19 14:05:02 +0200

    redux optimisation

---
+++ redux/optimizations-lect-5/src/App.jsx
@@ -0,0 +1,37 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from "./components/Layout";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/* catch all can be and 404 comp */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
