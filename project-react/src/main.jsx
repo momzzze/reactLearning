3dac32b1ec12d7795a4083605befd82e7afc1d1e
nikola.ninov <momzzze@abv.bg>
2024-03-31 18:53:32 +0300

    Update main.jsx

--- project-react/src/main.jsx
+++ project-react/src/main.jsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { seedLocalDatabase } from '@/api/data/seed';
 import ThemeProvider from '@/components/ThemeProvider';
+import { store } from '@/state/store';
 
-import App from './App';
 import Router from './Router';
 
 import './index.css';
@@ -13,6 +14,8 @@ seedLocalDatabase();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </ThemeProvider>,
 );
