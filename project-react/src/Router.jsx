55dc5a1c0f189316ce53a5bc3a34266ed67bc3f1
nikola.ninov <momzzze@abv.bg>
2024-03-31 18:53:53 +0300

    add ListingFavorites path to Router

--- project-react/src/Router.jsx
+++ project-react/src/Router.jsx
@@ -1,31 +1,39 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom"
 
 import App from "./App"
 import HomePage from "./pages/HomePage";
 import ListingDetailsPage from "./pages/ListingDetailsPage";
+import ListingFavoritesPage from "./pages/ListingFavoritesPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-const router = createBrowserRouter([
-    {
+const router = createBrowserRouter([{
+    path: "/",
+    element: < App />,
+    errorElement: <NotFoundPage />,
+    children: [{
         path: "/",
-        element: <App />,
-        // errorElement: <NotFoundPage />,
-        children:[
-            {
-                path: "/",
-                element: <HomePage />
-            },
-            {
-                path: "/listings/:listingId",
-                element: <ListingDetailsPage />
-            }
-        ],
+        element: < HomePage />
+    },
+    {
+        path: "/listings/:listingId",
+        element: < ListingDetailsPage />
+    },
+    {
+        path: "/favorites",
+        element: < ListingFavoritesPage />
     }
-]);
+    ],
+}]);
 
 const Router = () => {
-    return (
-        <RouterProvider router={router} />
+    return (<
+        RouterProvider router={
+            router
+        }
+    />
     )
 }
 
