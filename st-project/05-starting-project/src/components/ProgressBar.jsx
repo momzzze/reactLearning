74248e59b004eeead6901c4727becba3dd611bfc
momzzze <momzzze@abv.bg>
2026-03-05 18:26:41 +0200

    Initialize backend and frontend structure with Vite and React setup
    - Created backend package.json with dependencies for Express and body-parser.
    - Added index.html as the entry point for the frontend application.
    - Set up frontend package.json with scripts for development, build, and linting.
    - Included logo image in the public directory.
    - Developed main App component with state management for user places and modal handling.
    - Implemented AvailablePlaces component to fetch and display available places based on user location.
    - Created DeleteConfirmation component for confirming place removal actions.
    - Added Error component for displaying error messages.
    - Built Modal component for rendering modals using React portals.
    - Developed Places component to list user-selected places.
    - Created ProgressBar component for visual feedback during actions.
    - Implemented HTTP utility functions for fetching and updating places.
    - Added CSS styles for layout and design consistency.
    - Implemented location-based sorting for available places.
    - Set up main entry point for React application in main.jsx.
    - Configured Vite for React project.

---
+++ st-project/05-starting-project/src/components/ProgressBar.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+export const ProgressBar = ({ timer }) => {
+  const [remainingTime, setRemainingTime] = useState(timer);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <progress value={remainingTime} max={timer} />;
+};
